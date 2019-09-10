58f2876ca9f38e29d37f104f6445f1f861722010
MlsDmitry <d.molokovich@yandex.ru>
2019-09-10 19:04:36 +0300

    Update FTParticleUpdater.php
    title and text values incorrect position fix

--- src/mlsdmitry/FloatingText/task/FTParticleUpdater.php
+++ src/mlsdmitry/FloatingText/task/FTParticleUpdater.php
@@ -20,7 +20,7 @@ class FTParticleUpdater extends Task {
         foreach ($this->data as $name => $info) {
             $level = Server::getInstance()->getLevel($info['levelId']);
             if ($level) $level->loadChunk($info['coords'][0] >> 4, $info['coords'][2] >> 4);
-            $floatText = new FloatingTextParticle(new Position($info['coords'][0], $info['coords'][1], $info['coords'][2], $level), strval($info['title']), strval($info['text']));
+            $floatText = new FloatingTextParticle(new Position($info['coords'][0], $info['coords'][1], $info['coords'][2], $level), strval($info['text']), strval($info['title']));
             if ($level) $level->addParticle($floatText);
         }
     }
